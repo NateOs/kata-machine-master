56255856ddff7ce80b2de35053a38045df147cd4
nateos <sodjanathan@gmail.com>
2023-02-18 23:24:14 +0000

    feat: binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        const midpoint = Math.floor(lo + (hi - lo) / 2);
+        const v = haystack[midpoint];
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            hi = midpoint;
+        } else {
+            lo = midpoint + 1;
+        }
+    } while (lo < hi);
+    return false;
 }
